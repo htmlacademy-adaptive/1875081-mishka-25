861d5d31a445ea238cfe96fb8c9a8c5ddde639c3
Alyona <alenka.inform@gmail.com>
2022-04-11 20:32:27 +0300

    Adapt modal window

--- source/script.js
+++ source/script.js
@@ -5,3 +5,13 @@ button.onclick = function () {
   button.classList.toggle("main-nav__toggle--close");
   wrapper.classList.toggle('main-nav__menu--hidden');
 }
+
+
+let modalButton = document.querySelector(".product-card__button");
+let modalWindow = document.querySelector(".modal");
+let page = document.querySelector(".page");
+
+modalButton.onclick = function () {
+  modalWindow.classList.remove('modal--close');
+}
+
