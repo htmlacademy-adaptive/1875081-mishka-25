d7e85b695a18d127de6a9ac87275175436bd4bc7
Alyona <alenka.inform@gmail.com>
2022-04-13 15:52:35 +0300

    Adapt form

--- source/js/script-modal-index.js
+++ source/js/script-modal-index.js
@@ -1,11 +1,3 @@
-let button = document.querySelector('.main-nav__toggle');
-let wrapper = document.querySelector('.main-nav__menu');
-
-button.onclick = function () {
-  button.classList.toggle("main-nav__toggle--close");
-  wrapper.classList.toggle('main-nav__menu--hidden');
-}
-
 let modalButton = document.querySelector(".product-card__button");
 let modalWindow = document.querySelector(".modal");
 let modalContent = modalWindow.querySelector(".modal__content");
