3172ae2c163b39914236b7d47219c5faa7711a1e
Keks <keks@htmlacademy.ru>
2022-04-24 14:46:39 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/script-modal-index.js
@@ -0,0 +1,15 @@
+let modalButton = document.querySelector(".product-week__button");
+let modalWindow = document.querySelector(".modal");
+let modalContent = modalWindow.querySelector(".modal__content");
+
+modalContent.onclick = function(evt) {
+  evt.stopPropagation();
+}
+
+modalWindow.onclick = function(evt) {
+  modalWindow.classList.add('modal--close')
+}
+
+modalButton.onclick = function () {
+  modalWindow.classList.remove('modal--close');
+}
