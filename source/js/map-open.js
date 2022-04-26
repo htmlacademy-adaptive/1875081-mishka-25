02b8aaa696d780a95e6777b0fa382c390dfc65e1
Alyona <alenka.inform@gmail.com>
2022-04-26 12:40:09 +0300

    Improve the interactive map

---
+++ source/js/map-open.js
@@ -0,0 +1,5 @@
+let map = document.querySelector('.contacts-map__interactive');
+let mapImage = document.querySelector('.contacts-map__image-wrapper');
+
+map.classList.remove('contacts-map__interactive--nojs');
+mapImage.classList.add('contacts-map__image-wrapper--js');
