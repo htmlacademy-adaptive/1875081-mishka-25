39aa2d4e05fb51a261df907bbc29feed6ce46672
Keks <keks@htmlacademy.ru>
2022-05-01 11:45:42 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/script-burger.js
+++ 10/js/script-burger.js
@@ -1,11 +1,27 @@
 let button = document.querySelector('.main-nav__toggle');
-let wrapper = document.querySelector('.main-nav__menu');
+let listTop = document.querySelector('.main-nav__list');
+let listBottom = document.querySelector('.main-nav__users-menu');
+const mediaQuery = window.matchMedia('screen and (max-width: 767px)');
 
-button.classList.remove("main-nav__toggle--nojs");
-wrapper.classList.add("main-nav__menu--js");
-wrapper.classList.add("main-nav__menu--hidden");
+function handleMobileChange (l) {
+  if (l.matches) {
+    button.classList.remove('main-nav__toggle--nojs');
+    listTop.classList.add('main-nav__list--js');
+    listTop.classList.add('main-nav__list--hidden');
 
-button.onclick = function () {
-  button.classList.toggle("main-nav__toggle--close");
-  wrapper.classList.toggle('main-nav__menu--hidden');
+    while (listBottom.childNodes.length != 0) listTop.appendChild(listBottom.childNodes[0]);
+
+    button.onclick = function () {
+    button.classList.toggle('main-nav__toggle--close');
+    listTop.classList.toggle('main-nav__list--hidden');
+    }
+
+  } else {
+    button.classList.add('main-nav__toggle--nojs');
+    listTop.classList.remove('main-nav__list--js');
+    listTop.classList.remove('main-nav__list--hidden');
+  }
 }
+
+mediaQuery.addEventListener('change', handleMobileChange);
+handleMobileChange(mediaQuery);
