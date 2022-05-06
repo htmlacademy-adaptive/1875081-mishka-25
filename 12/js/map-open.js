3b8d7cc15f2b614edbbf9fd681740e9fb821e0a1
Keks <keks@htmlacademy.ru>
2022-05-06 09:37:26 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/map-open.js
@@ -0,0 +1,1 @@
+let map=document.querySelector(".contacts-map__interactive"),mapImage=document.querySelector(".contacts-map__image-wrapper");map.classList.remove("contacts-map__interactive--nojs"),mapImage.classList.add("contacts-map__image-wrapper--js");
